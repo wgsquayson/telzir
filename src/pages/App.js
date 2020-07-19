907c4fdddc3f0c1ba64aa77d64230d3e30ba2924
William Quayson <wgsquayson@gmail.com>
2020-07-18 23:03:43 -0300

    Adiçao do segundo banner e da divisoria, pendente alguns ajustes no css

--- src/pages/App.js
+++ src/pages/App.js
@@ -8,9 +8,34 @@ function App() {
       <Header />
       <div className={styles.content}>
         <div className={styles.banner}>
-          <p className={styles.beforeTitle}>Conheça nosso novo plano</p>
+          <p className={styles.beforeTitle}>Conheça nosso novo plano!</p>
           <p className={styles.title}>FALE</p>
           <p className={styles.title}>MAIS</p>
+        </div>
+        <div className={styles.bannerSplit}>
+          <p>Escolha o seu!</p>
+        </div>
+        <div className={styles.secondBanner}>
+          <div className={styles.leftSide}>
+
+          </div>
+          <div className={styles.offersContainer}>
+            <div className={styles.offersGroup}>
+              <p>FALE</p>
+              <p>MAIS</p>
+              <p>30</p>
+            </div>
+            <div className={styles.offersGroup}>
+              <p>FALE</p>
+              <p>MAIS</p>
+              <p>60</p>
+            </div>
+            <div className={styles.offersGroup}>
+              <p>FALE</p>
+              <p>MAIS</p>
+              <p>120</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
